a23b38d0010f1346bdebfc948a6bc74b96ba26c5
BehruzTurgunov <behruzturgunov2006@gmail.com>
2025-07-22 13:24:41 +0500

    A minor error has been fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
-
-import './globals.css' 
+import './globals.css'
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import type { ReactNode } from 'react'
+
 export const metadata = {
   title: 'Mening Sahifam',
   description: 'Next.js App Router bilan tayyorlangan sahifa',
 }
 
-export default function RootLayout({ children }) {
+type RootLayoutProps = {
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="uz">
       <body className='flex flex-col w-full min-h-screen'>
-        <Header />       
-        <main className='grow'>{children}</main> 
-        <Footer/>
+        <Header />
+        <main className='grow'>{children}</main>
+        <Footer />
       </body>
     </html>
   )
